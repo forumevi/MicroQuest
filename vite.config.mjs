1b58cd4fda86d393a6c33d608ecf6942e2891a35
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-13 22:07:07 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Vite config (ESM format)
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext'
-  }
+    target: 'esnext',
+    rollupOptions: {
+      // Farcaster SDK şu an npm’de yok, bu nedenle Vite derleme sırasında yokmuş gibi davranacak
+      external: ['@farcaster/miniapp-sdk'],
+    },
+  },
 });
