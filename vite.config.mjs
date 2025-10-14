0fc9ed10b7f7cefd1637cd9b5dc36c77b571a821
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-14 20:34:03 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: { target: "esnext" }
+  build: { target: "es2020" } // esnext yerine es2020
 });
