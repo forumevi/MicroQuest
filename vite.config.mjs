84a68ce37043b327e78a8fff556acbf21c95c3e4
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-14 20:26:21 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,18 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: [] // boş bırakıyoruz; dinamik import'u *vite-ignore* ile kontrol edeceğiz
-  },
-  resolve: {
-    // alias gerekirse ekle
-  },
   build: {
-    target: 'esnext',
-    rollupOptions: {
-      // bunları boş bırakıyoruz; dışarı alma yerine import'u runtime'a bırakacağız
-    }
+    target: "esnext",
+    rollupOptions: {}
   }
 });
