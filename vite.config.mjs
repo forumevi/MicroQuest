88fefbe534d36720efedf49d7ebfe5f535055e5a
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-14 20:27:55 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: "esnext",
-    rollupOptions: {}
-  }
+  build: { target: "esnext" }
 });
